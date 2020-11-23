4b3dffb7ad128ed2acdb8dfd96065ede30e635a1
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-23 11:53:17 -0700

    commit tree c9600fd1cae4795ccbb35138e95b93ad6b73c3f4

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,17 @@
 def starts_with_a_vowel?(word)
+    first_letter = word[0]
 
+    if first_letter.match(/[bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ]/)
+        return false
+    elsif first_letter.match(/[aeiouAEIOU]/)
+        return true
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+    array = text.scan(/ing$/)
+    return array.count
+
 
 end
 
